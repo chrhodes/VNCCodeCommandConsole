1f4cab411220c67c1ea3110f2f023a80db41bbbd
Christopher Rhodes <chrhodes@cox.net>
2021-04-12 08:48:12 -0700

    m

--- CCC.CodeChecks/Properties/AssemblyInfo.cs
+++ CCC.CodeChecks/Properties/AssemblyInfo.cs
@@ -86,4 +86,4 @@ using System.Windows;
 //  1.0.0.0   yyyy/mm/dd    Developer
 //      Initial Version
 
-[assembly: AssemblyInformationalVersion("1.0.0.0-2021-04-04A")]
+[assembly: AssemblyInformationalVersion("1.0.0.0-2021-04-10A")]
