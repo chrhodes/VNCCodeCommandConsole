b54cfabb87e8c809044492a3e462fb52d3822b7c
Christopher Rhodes <chrhodes@cox.net>
2021-04-26 09:09:45 -0700

    m

---
+++ VNCCodeCommandConsoleTestFiles/ExploreClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace foo
+{ 
+	public class ClassPublic
+	{
+		public ClassPublic()
+		{
+		}
+		public void Method1()
+        {
+            
+        }
+    }
+
+	private class ClassPrivate
+	{
+		public ClassPrivate()
+		{
+		}
+
+		private void Method1()
+		{
+
+		}
+	}
+
+	protected class ClassProtected
+	{
+		public ClassProtected()
+		{
+		}
+
+		protected void Method1()
+		{
+
+		}
+	}
+
+	internal class ClassInternal
+	{
+		public ClassInternal()
+		{
+		}
+
+		internal void Method1()
+		{
+
+		}
+	}
+}
+
---
+++ VNCCodeCommandConsoleTestFiles/Roslyn-SyntaxTreeBasics.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Namespace1
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        private bool PrivateMethodBool()
+        {
+
+        }
+
+        private string PrivateMethodString()
+        {
+
+        }
+
+        private void PrivateMethodVoid()
+        {
+
+        }
+
+        internal void InternalMethodVoid()
+        {
+
+        }
+
+        protected internal void ProtectedInternalMethodVoid()
+        {
+
+        }
+
+        protected void ProtectedMethodVoid()
+        {
+
+        }
+        public void PublicMethodVoid()
+        {
+
+        }
+
+        private class Class1a
+        {
+
+        }
+
+        private class Class1b
+        {
+            private class Class1b1
+            {
+
+            }
+        }
+    }
+
+    namespace NestedNamespace
+    {
+        using VNC;
+
+        public class ClassOne
+        {
+            
+            public ClassOne()
+            {
+                void Method1()
+                {
+                    
+                }
+
+                void Method2()
+                {
+                    
+                }
+            }
+
+            private class ClassOneA
+            {
+
+            }
+
+            private class ClassOneB
+            {
+                private class ClassOneBOne
+                {
+
+                }
+            }
+        }
+    }
+}
